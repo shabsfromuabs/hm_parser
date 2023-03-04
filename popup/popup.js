9b719d56b827db41a01b6ee844dd354e54d48361
Artem Shablii <artem.shablii@thredup.com>
2023-03-04 18:35:30 +0100

    manifest upgrade

--- popup/popup.js
+++ popup/popup.js
@@ -1,87 +1,119 @@
 class Popup {
   constructor() {
-    this.initializeButton = document.querySelector('#initialize');
-    this.parseButton = document.querySelector('#parse');
-    this.openHmButton = document.querySelector('#open-hm');
-    this.credentialsForm = document.querySelector('#credentials-form');
+    this.initializeButton = document.querySelector("#initialize");
+    this.parseButton = document.querySelector("#parse");
+    this.openHmButton = document.querySelector("#open-hm");
+    this.credentialsForm = document.querySelector("#credentials-form");
     this.setUiElements();
     this.addEventListeners();
   }
 
-  // Set button text and shows/hides UI elements
-  setUiElements() {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const currentTab = tabs[0];
-
-      if (currentTab.url && currentTab.url.startsWith('https://app.hmbee.ru/app')) {
-        this.initializeButton.innerText = 'Initialize uploader';
-        this.parseButton.classList.add("d-none");
-        this.openHmButton.classList.add("d-none");
-        this.credentialsForm.classList.remove("d-none");
-        this.loadCredentials();
-      }
-    });
+  async getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
   }
 
-  addEventListeners() {
+  // Set button text and shows/hides UI elements
+  async setUiElements() {
+    let currentTab = await this.getCurrentTab();
+
+    if (currentTab.url?.startsWith("https://app.hmbee.ru/app")) {
+      this.initializeButton.innerText = "Initialize uploader";
+      this.parseButton.classList.add("d-none");
+      this.openHmButton.classList.add("d-none");
+      this.credentialsForm.classList.remove("d-none");
+      this.loadCredentials();
+    }
+  }
+
+  async addEventListeners() {
     // Includes scripts on the page
-    this.initializeButton.addEventListener('click', () => this.addContentScripts());
+    this.initializeButton.addEventListener("click", () =>
+      this.addContentScripts()
+    );
+
+    let currentTab = await this.getCurrentTab();
 
     // Send "parse" command to content script on page
-    this.parseButton.addEventListener('click', () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { command: 'parse' });
-      });
+    this.parseButton.addEventListener("click", () => {
+      chrome.tabs.sendMessage(currentTab.id, { command: "parse" });
     });
 
     // Save credentials on form submit
-    this.credentialsForm.addEventListener('submit', (e) => {
+    this.credentialsForm.addEventListener("submit", (e) => {
       e.preventDefault();
       const data = new FormData(this.credentialsForm);
-      chrome.storage.local.set({ credentials: {
-        email: data.get('email'), password: data.get('password') }
+      chrome.storage.local.set({
+        credentials: {
+          email: data.get("email"),
+          password: data.get("password"),
+        },
       });
     });
   }
 
   // Loads email and password of HM account and sets to from fields
   loadCredentials() {
-    chrome.storage.local.get('credentials', (result) => {
+    chrome.storage.local.get("credentials", (result) => {
       const credentials = result.credentials || {};
-      this.credentialsForm.querySelector('input[name="email"]').value = credentials.email || null;
-      this.credentialsForm.querySelector('input[name="password"]').value = credentials.password || null;
+      this.credentialsForm.querySelector('input[name="email"]').value =
+        credentials.email || null;
+      this.credentialsForm.querySelector('input[name="password"]').value =
+        credentials.password || null;
     });
   }
 
   // Add proper content script to currently open page
-  addContentScripts() {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const currentTab = tabs[0];
-      chrome.tabs.executeScript(currentTab.id, { file: 'includes/common/helpers.js' });
-      chrome.tabs.executeScript(currentTab.id, { file: 'includes/common/accounts.js' });
-      chrome.tabs.executeScript(currentTab.id, { file: 'includes/common/categories.js' });
+  async addContentScripts() {
+    const currentTab = await this.getCurrentTab();
+    const target = { tabId: currentTab.id };
 
-      if (currentTab.url && currentTab.url.startsWith('https://online.ukrsibbank.com/ibank')) {
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/parser/ukrsibParser.js' });
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/parser/parse.js' });
-      } else if (currentTab.url && currentTab.url.startsWith('https://my.alfabank.com.ua')) {
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/parser/alfaParser.js' });
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/parser/parse.js' });
-      } else if (currentTab.url && currentTab.url.startsWith('https://wise.com/balances/')) {
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/parser/wiseParser.js' });
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/parser/parse.js' });
-      } else if (currentTab.url && currentTab.url.startsWith('https://www.bankmillennium.pl/')) {
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/parser/millenniumParser.js' });
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/parser/parse.js' });
-      } else if (currentTab.url && currentTab.url.startsWith('https://app.hmbee.ru/app')) {
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/hm/autocompleter.js' });
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/hm/uiHelpers.js' });
-        chrome.tabs.executeScript(currentTab.id, { file: 'includes/hm/helpers.js' });
-        chrome.tabs.executeScript(tabs[0].id, { file: 'includes/hm/hm.js' });
-        // Add styling
-        chrome.tabs.insertCSS(tabs[0].id, { file: 'includes/hm/styles.css' });
-      }
+    chrome.scripting.executeScript({
+      target,
+      files: [
+        "includes/common/helpers.js",
+        "includes/common/accounts.js",
+        "includes/common/categories.js",
+      ],
     });
+
+    if (currentTab.url?.startsWith("https://online.ukrsibbank.com/ibank")) {
+      const files = [
+        "includes/parser/ukrsibParser.js",
+        "includes/parser/parse.js",
+      ];
+      chrome.scripting.executeScript({ target, files });
+    } else if (currentTab.url?.startsWith("https://my.alfabank.com.ua")) {
+      const files = [
+        "includes/parser/alfaParser.js",
+        "includes/parser/parse.js",
+      ];
+      chrome.scripting.executeScript({ target, files });
+    } else if (currentTab.url?.startsWith("https://wise.com/balances/")) {
+      const files = [
+        "includes/parser/wiseParser.js",
+        "includes/parser/parse.js",
+      ];
+      chrome.scripting.executeScript({ target, files });
+    } else if (currentTab.url?.startsWith("https://www.bankmillennium.pl/")) {
+      const files = [
+        "includes/parser/millenniumParser.js",
+        "includes/parser/parse.js",
+      ];
+      chrome.scripting.executeScript({ target, files });
+    } else if (currentTab.url?.startsWith("https://app.hmbee.ru/app")) {
+      const files = [
+        "includes/hm/autocompleter.js",
+        "includes/hm/uiHelpers.js",
+        "includes/hm/helpers.js",
+        "includes/hm/hm.js",
+      ];
+      chrome.scripting.executeScript({ target, files });
+      // Add styling
+      chrome.scripting.insertCSS({ target, files: ["includes/hm/styles.css"] });
+    }
   }
 }
 
